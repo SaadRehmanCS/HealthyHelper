a7498c0709d265476203f68cd8cde55b74bdbd0b
saad <saadrehman2000@gmail.com>
2021-02-13 05:54:13 +0400

    phase 1 completed at minimum level

--- src/main/model/Water.java
+++ src/main/model/Water.java
@@ -1,15 +1,21 @@
 package model;
 
-//this class constructs a water object
+//this class constructs a water object, to which more water can
+//be added, as long as it is below the daily requirement
 public class Water {
 
     private static final int DAILY_REQUIREMENT = 8;
     private int amountConsumed;
 
+    //MODIFIES: this
+    //EFFECTS: initializes the amount consumed
     public Water() {
         amountConsumed = 0;
     }
 
+    //MODIFIES: this
+    //EFFECTS: increments the amount consumed as long as it is less
+    //      than the daily requirement limit
     public void incrementWater() {
         if (getAmountConsumed() < DAILY_REQUIREMENT) {
             amountConsumed++;
@@ -20,6 +26,9 @@ public class Water {
         return amountConsumed;
     }
 
+    //MODIFIES: this
+    //EFFECTS: increments the amount consumed by the number of cups in
+    //      the parameter
     public void setAmountConsumed(int cups) {
         amountConsumed += cups;
     }
