7b31c642df8f9173e24191e552a291249ee33baa
saad <saadrehman2000@gmail.com>
2021-03-18 03:17:44 +0400

    implemented more gui

--- src/main/gui/NewUserPanel.java
+++ src/main/gui/NewUserPanel.java
@@ -1,13 +1,96 @@
 package gui;
 
+import model.DietPlan;
+import model.exceptions.ImpossibleBodyDimensionsException;
+
 import javax.swing.*;
+import java.util.InputMismatchException;
 
 public class NewUserPanel extends JPanel {
+
+    JLabel welcomeMsg;
+    JLabel heightLabel;
+    JTextField heightText;
+    JLabel weightLabel;
+    JTextField weightText;
+    JButton enterButton;
+    private DietPlan dietPlan;
+    JLabel exceptionMsg;
+    JLabel bmiInfo;
+    JButton cutBtn;
+    JButton bulkBtn;
+    JButton maintainBtn;
+
 
     public NewUserPanel() {
 
         new JPanel();
         setLayout(null);
+        promptUserInformation();
+
+    }
+
+    private void promptUserInformation() {
+        welcomeMsg = new JLabel("<html><body><b>Welcome to HealthyHelper!</b> the application "
+                + " that can help you keep your fitness<br> goals on track! "
+                + "Lets begin by entering your height and weight below.<body></html>");
+        add(welcomeMsg);
+        welcomeMsg.setBounds(50, 20, 1000, 50);
+
+        heightLabel = new JLabel("Height(cm): ");
+        add(heightLabel);
+        heightLabel.setBounds(10, 100, 120, 25);
+
+        heightText = new JTextField();
+        add(heightText);
+        heightText.setBounds(80, 100, 100, 25);
+
+        weightLabel = new JLabel("Weight(kg): ");
+        add(weightLabel);
+        weightLabel.setBounds(10, 150, 120, 25);
+
+        weightText = new JTextField();
+        add(weightText);
+        weightText.setBounds(80, 150, 100, 25);
+
+        exceptionMsg = new JLabel("");
+        add(exceptionMsg);
+        exceptionMsg.setBounds(80, 180, 300, 25);
+        handleUserInput();
+    }
+
+    public void handleUserInput() {
+        enterButton = new JButton("Calculate");
+        add(enterButton);
+        enterButton.setBounds(40, 210, 100, 60);
+        enterButton.addActionListener(e -> {
+            try {
+                double height = Double.parseDouble(heightText.getText());
+                double weight = Double.parseDouble(weightText.getText());
+                dietPlan = new DietPlan(height, weight);
+                exceptionMsg.setText("");
+                bmiInfo = new JLabel();
+                bmiInfo.setText("<html><body>Your BMI comes out to: " + dietPlan.calculateBMI()
+                        + ".<br>According to our estimates, you are classified as " + dietPlan.bmiAssessment()
+                        + ".<br> Based on this, we recommend that you choose the <b>" + dietPlan.dietPlanRecommendation()
+                        + "</b> strategy<br> to achieve the best results</body></html>");
+                bmiPlanPicker();
+            } catch (NumberFormatException exception) {
+                exceptionMsg.setText("Please enter numerical values");
+            } catch (ImpossibleBodyDimensionsException exception) {
+                exceptionMsg.setText("These values are invalid");
+            }
+
+        });
+
+    }
+
+    private void bmiPlanPicker() {
+        add(bmiInfo);
+        bmiInfo.setBounds(80, 290, 1000, 100);
+        cutBtn = new JButton("Cut");
+        bulkBtn = new JButton("Bulk");
+        maintainBtn = new JButton("Maintain");
 
     }
 }
--- src/main/gui/ProgramFrame.java
+++ src/main/gui/ProgramFrame.java
@@ -1,18 +1,33 @@
 package gui;
 
+import model.DietPlan;
+import persistence.JsonReader;
+import persistence.JsonWriter;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
 import java.net.URL;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class ProgramFrame extends JFrame {
 
+    private static final String JSON_STORE = "./data/user.json";
     JPanel startPanel;
     JLabel startPrompt;
     JButton startBtn;
     JLabel returnPrompt;
     JButton returnBtn;
+    JsonWriter jsonWriter;
+    JsonReader jsonReader;
+    protected static int day;
+    protected static final Calendar CALENDER = Calendar.getInstance(TimeZone.getDefault());
+
+
+
 
     public ProgramFrame() {
         super("Healthy Helper");
@@ -24,6 +39,8 @@ public class ProgramFrame extends JFrame {
         add(createStartPanel());
         setVisible(true);
         setResizable(false);
+        jsonWriter = new JsonWriter(JSON_STORE);
+        jsonReader = new JsonReader(JSON_STORE);
     }
 
     public void setImageIcon() {
@@ -44,21 +61,18 @@ public class ProgramFrame extends JFrame {
         constructJLabel(startPrompt, "Click start to begin tracking!", 50);
 
         constructJButton(1, "Start", 80);
-        startBtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                replacePanel(startPanel, new NewUserPanel());
-            }
+        startBtn.addActionListener(e -> {
+            clearUserData();
+            day = CALENDER.get(Calendar.DATE);
+            NewUserPanel newUserPanel = new NewUserPanel();
+            replacePanel(startPanel, newUserPanel);
         });
 
         constructJLabel(returnPrompt, "Otherwise, click here to continue your journey", 180);
 
         constructJButton(2, "Continue", 220);
-        returnBtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //replacePanel(startPanel, new NewUserPanel());
-            }
+        returnBtn.addActionListener(e -> {
+            //replacePanel(startPanel, new NewUserPanel());
         });
 
         return startPanel;
@@ -87,6 +101,15 @@ public class ProgramFrame extends JFrame {
         remove(oldPanel);
         add(newPanel);
         setVisible(true);
+    }
+
+    private void clearUserData() {
+        try {
+            jsonWriter.open();
+            jsonWriter.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File does not exist");
+        }
     }
 
     public static void main(String[] args) {
